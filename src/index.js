d0e0963e5cf6612a833349e0c760f0cc47161c5d
danielbatres <danielbatresuser@gmail.com>
2024-01-06 16:30:45 -0600

    Add log middleware

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { pokemonsReducer } from './reducers/pokemons';
 import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
+import { 
+  applyMiddleware, 
+  compose, 
+  legacy_createStore as createStore 
+} from "redux";
 import './index.css';
 import App from './App';
+import { logger } from './middlewares';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const store = createStore(
-  pokemonsReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhacers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger)
 );
+
+const store = createStore(pokemonsReducer, composeEnhacers);
 
 root.render(
   <React.StrictMode>
