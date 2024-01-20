47c1850b1467408b4db228865745fe4a27bbaa18
danielbatres <danielbatresuser@gmail.com>
2024-01-19 23:23:36 -0600

    Add types

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,13 +3,15 @@ import { Card } from "antd";
 import { Meta } from "antd/es/list/Item";
 import "./PokemonList.css";
 
-const PokemonCard = ({ name, image }) => {
+const PokemonCard = ({ name, image, types }) => {
+  const typesString = types.map(item => item.type.name).join(", ");
+
   return <Card
     title={name}
     cover={<img src={image} alt={name} />}
     extra={<StarOutlined />}
   >
-    <Meta description="fire, magic" />
+    <Meta description={typesString} />
   </Card>
 }
 
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -4,7 +4,12 @@ const PokemonList = ({ pokemons }) => {
   return (
     <div className="PokemonList">
       {pokemons?.map(pokemon => (
-        <PokemonCard name={pokemon.name} key={pokemon.name} image={pokemon.sprites.front_default} />
+        <PokemonCard 
+          name={pokemon.name} 
+          key={pokemon.name} 
+          image={pokemon.sprites.front_default} 
+          types={pokemon.types}
+        />
       ))}
     </div>
   );
