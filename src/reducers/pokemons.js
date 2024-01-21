42984fa3b089db61f9605c4fc5a534c0350bb93e
danielbatres <danielbatresuser@gmail.com>
2024-01-21 16:00:28 -0600

    Separate reducers

--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,9 +1,8 @@
 import { fromJS } from "immutable";
-import { SET_FAVORITE, SET_LOADING, SET_POKEMONS } from "../actions/types"
+import { SET_FAVORITE, SET_POKEMONS } from "../actions/types"
 
 const initialState = fromJS({
-  pokemons: [],
-  loading: false
+  pokemons: []
 });
 
 export const pokemonsReducer = (state = initialState, action) => {
@@ -19,8 +18,6 @@ export const pokemonsReducer = (state = initialState, action) => {
       const isFavorite = state.getIn(["pokemons",currentPokemonIndex, "favorite"]);
 
       return state.setIn(["pokemons", currentPokemonIndex, "favorite"], !isFavorite);
-    case SET_LOADING:
-      return state.setIn(["loading"], action.payload);
     default: 
       return state;
   }
