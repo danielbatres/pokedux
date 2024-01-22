7578c5d1fa5118d90b461712060503b479061533
danielbatres <danielbatresuser@gmail.com>
2024-01-22 14:05:21 -0600

    Add data slice

---
+++ src/slices/dataSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  pokemons: [],
+};
+
+export const dataSlice = createSlice({
+  name: "data",
+  initialState,
+  reducers: {
+    setPokemons: (state, action) => {
+      state.pokemons = action.payload;
+    },
+    setFavorite: (state, action) => {
+      const currentPokemonIndex = state.pokemons.findIndex(pokemon => pokemon.id === action.payload.pokemonId);
+
+      if (currentPokemonIndex >= 0) {
+        const isFavorite = state.pokemons[currentPokemonIndex].favorte;
+
+        state.pokemons[currentPokemonIndex].favorte = !isFavorite;
+      }
+    }
+  }
+});
+
+export const { setFavorite, setPokemons } = dataSlice.actions;
+
+export default dataSlice.reducer;
